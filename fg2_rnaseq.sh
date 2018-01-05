9caeb1b928f7ac3bc0759c3089d29d9455c4b253
mahiarmahjoub <31858737+mahiarmahjoub@users.noreply.github.com>
2018-01-05 17:40:25 +0000

    Update fg2_rnaseq.sh

--- fg2_rnaseq.sh
+++ fg2_rnaseq.sh
@@ -12,14 +12,14 @@ bowtie-build --offrate 3 Homo_sapiens.GRCh37.70.ref_transcripts.fa Homo_sapiens.
 # loop over all fastq files 
 # trim -> align -> annotate alignment -> mmseq expression quantification 
 
-for f in *fastq.gz 
+for f in *.fastq
 do 
   echo "Analysing file $f ..."
   # Step 1c: Trim out adapter sequences if necessary
   trim_galore -q 15 --stringency 3 -e 0.05 --length 36 --trim1 $f 
   # Step 2a: Align reads with Bowtie 1 (not Bowtie 2) 
-  bowtie -a --best --strata -S -m 100 -X 500 --chunkmbs 256 -p 8 Homo_sapiens.GRCh37.70.ref_transcripts \
-  -1 $f | samtools view -F 0xC -bS - | \
+  bowtie -a --best --strata -S -m 100 --chunkmbs 256 -p 8 Homo_sapiens.GRCh37.70.ref_transcripts \
+  $f | samtools view -F -bS - | \
   samtools sort -n - $f.namesorted
   # Step 3: Map reads to transcript sets 
   bam2hits Homo_sapiens.GRCh37.70.ref_transcripts.fa $f.namesorted.bam > $f.hits
